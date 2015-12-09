e8d0c7a073bbad7cb4933d51fb6d1fdea6628df0
Jusive <1345266022@qq.com>
2015-12-09 14:10:05 +0800

    dispatch_asyncDownlodingimage
    异步下载图片

--- 异步下载图片/ViewController.m
+++ 异步下载图片/ViewController.m
@@ -9,19 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong) UIScrollView *scV;
+@property (nonatomic,strong) UIImageView *imageV;
 @end
 
 @implementation ViewController
 
+
+-(void)loadView{
+    self.scV = [[UIScrollView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.scV.backgroundColor = [UIColor whiteColor];
+    self.view  =  self.scV;
+    
+    self.imageV = [[UIImageView alloc]init];
+    [self.scV addSubview:self.imageV];
+    
+    
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSURL *URL = [NSURL URLWithString:@"http://g.hiphotos.baidu.com/image/pic/item/472309f790529822c4ac8ad0d5ca7bcb0a46d402.jpg"];
+        NSData *data = [NSData dataWithContentsOfURL:URL];
+        //转换成图片
+        UIImage * image = [UIImage imageWithData:data];
+        //回到主线程
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            self.imageV.image = image;
+            [self.imageV sizeToFit];
+            self.scV.contentSize = image.size;
+        });
+    });
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
